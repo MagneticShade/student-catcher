8d1d528b423078b7572b2d245c57380c77f97941
Sergey Astapenko <mclotos@xakep.ru>
2022-10-07 23:37:09 +0200

    add Human entity

---
+++ Entities/Human.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace College\Entities;
+
+class Human {
+    private ?string $middleName = null;
+
+    public function __construct(
+        private string $firstName,
+        private string $lastName,
+        private DateTime $birthDate,
+        private string $gender
+    ) {}
+
+    public function getFirstName() : string
+    {
+        return ucfirst(strtolower($this->firstName));
+    }
+
+    public function getMiddleName() : ?string
+    {
+        return ucfirst(strtolower($this->middleName));
+    }
+
+    public function getLastName() : string
+    {
+        return ucfirst(strtolower($this->lastName));
+    }
+
+    public function getBirthDate() : DateTime
+    {
+        return $this->birthDate->format('d-m-Y');
+    }
+
+    public function getGender() : string
+    {
+        return $this->gender;
+    }
+
+    public function changeFirstName(string $newFitstName) : void
+    {
+        $this->firstName = $newFitstName;
+    }
+
+    public function changeLastName(string $newLastName) : void
+    {
+        $this->lastName = $newLastName;
+    }
+
+    public function setMiddleName(string $middleName) : void
+    {
+        $this->middleName = $middleName;
+    }
+
+    public function getFullName() : string
+    {
+        return implode(' ',
+            [
+                $this->getLastName(),
+                $this->getFirstName(),
+                $this->getMiddleName(),
+            ]
+        );
+    }
+
+    public function getInitials() : string
+    {
+        return $this->getLastName() .' ' .
+            mb_substr($this->getFirstName(), 0, 1) . '.'
+            $this->getLastName() === null
+                ? null
+                : ' ' . mb_substr($this->getLastName(), 0, 1) . '.';
+    }
+}
