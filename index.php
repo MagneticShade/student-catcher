0798494b9edc007fb3aca08f729db2ab5bf404c9
McLotos <mclotos@gmail.com>
2022-10-08 09:11:21 +0200

    fix typo
    DIRECTORY_SEPRATOR => DIRECTORY_SEPARATOR

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    require __DIR__ . DIRECTORY_SEPRATOR . implode(
+    require __DIR__ . DIRECTORY_SEPARATOR . implode(
         DIRECTORY_SEPARATOR,
         explode('\\', str_replace('College', '', $class))
     ) . '.php';
