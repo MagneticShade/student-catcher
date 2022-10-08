b5693dfbd0fe82b526a8738d72b9508739723cd5
MagneticShade <senya.krivorot11@gmail.com>
2022-10-08 16:44:41 +0200

    enough

--- Entities/Curator.php
+++ Entities/Curator.php
@@ -2,7 +2,17 @@
 
 namespace College\Entities;
 
+use DateTime;
+
 class Curator extends Human
 {
+public function __construct(string $firstName, string $lastName, DateTime $birthDate, string $gender)
+{
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+    $this->birthDate = $birthDate;
+    $this->gender = $gender;
+    parent::__construct($firstName,$lastName,$birthDate,$gender);
+}
 
 }
--- Entities/Student.php
+++ Entities/Student.php
@@ -1,8 +1,30 @@
 <?php
 
 namespace College\Entities;
-
+use DateTime;
 class Student extends Human
 {
+    public function __construct(
 
+        private Human $human,private string $group,private DateTime $adm_year,private Curator $curator,string $firstName, string $lastName, DateTime $birthDate, string $gender){
+
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->birthDate = $birthDate;
+        $this->gender = $gender;
+        parent::__construct($firstName,$lastName,$birthDate,$gender);
+    }
+    public function getHuman():Human{
+        return $this->human;
+    }
+    public function getGroup():string{
+        return $this->group;
+    }
+    public function getCurator():Curator{
+        return $this->curator;
+    }
+    public function getAdmYear():DateTime
+{
+        return $this->adm_year;
+}
 }
